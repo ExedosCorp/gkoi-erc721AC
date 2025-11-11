be15870fa63d1fe28dffbfeccce51d74acafc692
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2025-11-11 20:06:38 +0100

    Refactor GKoiBattleCards contract: improve formatting and add base URI functionality

--- src/GKoiBattleCards.sol
+++ src/GKoiBattleCards.sol
@@ -3,11 +3,15 @@ pragma solidity ^0.8.30;
 
 import {OwnableBasic} from "@limitbreak/creator-token-standards/src/access/OwnableBasic.sol";
 import {ERC721AC} from "@limitbreak/creator-token-standards/src/erc721c/ERC721AC.sol";
-import {BasicRoyalties, BasicRoyaltiesBase} from "@limitbreak/creator-token-standards/src/programmable-royalties/BasicRoyalties.sol";
+import {
+    BasicRoyalties,
+    BasicRoyaltiesBase
+} from "@limitbreak/creator-token-standards/src/programmable-royalties/BasicRoyalties.sol";
 import {ERC2981} from "@openzeppelin/contracts/token/common/ERC2981.sol";
 
 contract GKoiBattleCards is OwnableBasic, ERC721AC, ERC2981, BasicRoyalties {
     string private _contractUri;
+    string private _baseUri;
     uint256 public constant MAX_SUPPLY = 10_000;
 
     modifier checkMaxSupply(uint256 quantity) {
@@ -21,9 +25,7 @@ contract GKoiBattleCards is OwnableBasic, ERC721AC, ERC2981, BasicRoyalties {
         string memory name_,
         string memory symbol_,
         string memory _contractUri_
-    ) 
-        ERC721AC(name_, symbol_) 
-        BasicRoyalties(royaltyReceiver_, royaltyFeeNumerator_) {
+    ) ERC721AC(name_, symbol_) BasicRoyalties(royaltyReceiver_, royaltyFeeNumerator_) {
         _contractUri = _contractUri_;
     }
 
@@ -35,16 +37,23 @@ contract GKoiBattleCards is OwnableBasic, ERC721AC, ERC2981, BasicRoyalties {
         return _contractUri;
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721AC, ERC2981) returns (bool) {
-        return ERC721AC.supportsInterface(interfaceId) ||
-            ERC2981.supportsInterface(interfaceId);
+    function _baseURI() internal view override returns (string memory) {
+        return _baseUri;
     }
 
-    function mint(address to, uint256 quantity) external onlyOwner() checkMaxSupply(quantity) {
+    function setBaseURI(string memory baseURI_) external onlyOwner {
+        _baseUri = baseURI_;
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721AC, ERC2981) returns (bool) {
+        return ERC721AC.supportsInterface(interfaceId) || ERC2981.supportsInterface(interfaceId);
+    }
+
+    function mint(address to, uint256 quantity) external onlyOwner checkMaxSupply(quantity) {
         _mint(to, quantity);
     }
 
-    function safeMint(address to, uint256 quantity) external onlyOwner() checkMaxSupply(quantity) {
+    function safeMint(address to, uint256 quantity) external onlyOwner checkMaxSupply(quantity) {
         _safeMint(to, quantity);
     }
 
@@ -61,11 +70,10 @@ contract GKoiBattleCards is OwnableBasic, ERC721AC, ERC2981, BasicRoyalties {
         _setDefaultRoyalty(receiver, feeNumerator);
     }
 
-    function _setTokenRoyalty(
-        uint256 tokenId,
-        address receiver,
-        uint96 feeNumerator
-    ) internal override(BasicRoyaltiesBase, ERC2981) {
+    function _setTokenRoyalty(uint256 tokenId, address receiver, uint96 feeNumerator)
+        internal
+        override(BasicRoyaltiesBase, ERC2981)
+    {
         _requireCallerIsContractOwner();
         super._setTokenRoyalty(tokenId, receiver, feeNumerator);
     }
