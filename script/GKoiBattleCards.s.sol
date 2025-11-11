2e24b927a3eb34f2959771dd49e9ed1be623e278
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2025-11-11 20:28:50 +0100

    Fix casing of GKoiBattlecards references in deployment script

--- script/GKoiBattleCards.s.sol
+++ script/GKoiBattleCards.s.sol
@@ -2,21 +2,21 @@
 pragma solidity ^0.8.30;
 
 import {Script, console} from "forge-std/Script.sol";
-import {GKoiBattleCards} from "../src/GKoiBattleCards.sol";
+import {GKoiBattlecards} from "../src/GKoiBattlecards.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
-contract DeployGKoiBattleCards is Script {
-    function run() external returns (GKoiBattleCards) {
+contract DeployGKoiBattlecards is Script {
+    function run() external returns (GKoiBattlecards) {
         HelperConfig config = new HelperConfig();
         (uint256 deployerPrivateKey, address royaltyReceiver, uint96 royalty, string memory contractURI) =
             config.activeNetworkConfig();
 
         vm.startBroadcast(deployerPrivateKey);
-        GKoiBattleCards gKoiBattleCards =
-            new GKoiBattleCards(royaltyReceiver, royalty, "GKoi BattleCards", "GKOI", contractURI);
+        GKoiBattlecards gKoiBattlecards =
+            new GKoiBattlecards(royaltyReceiver, royalty, "GKoi Battlecards", "GKOI", contractURI);
         vm.stopBroadcast();
 
-        console.log("GKoiBattleCards deployed to:", address(gKoiBattleCards));
-        return gKoiBattleCards;
+        console.log("GKoiBattlecards deployed to:", address(gKoiBattlecards));
+        return gKoiBattlecards;
     }
 }
